b10378ef512a5c24b6fc945254660836860a0260
gulamd <gulamrasool78655@gmail.com>
2019-05-12 20:50:14 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -7,7 +7,7 @@ do
 	echo "=============================="
     echo "Creating www.gamutkart$i container.."
     sleep 1
-    echo "india@123456789" | sudo -S docker run --name www.gamutkart$i -d -it --rm rgulam94/p13 /bin/bash
+    echo "india@123456789" | sudo -S docker run --name www.gamutkart$i -d -it --rm rgulam94/png /bin/bash
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done	  
