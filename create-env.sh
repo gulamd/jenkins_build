13d2d55e618f0c2bfbfc1873bdff71913ec9f4e0
gulamd <gulamrasool78655@gmail.com>
2019-05-12 20:15:38 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -10,5 +10,3 @@ do
     echo "india@123456789" | sudo -S docker run --name www.gamutkart$i -d -it --rm rgulam94/png12 /bin/bash
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
-done
-echo "india@123456789" | sudo -S docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
