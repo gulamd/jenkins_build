7eebd9246ec8e093d104da0db1014b090cff7338
gulamd <gulamrasool78655@gmail.com>
2019-07-11 11:34:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 ##
 ##
 ENVIRONMENT=$1
-if [ $ENVIRONMENT = "QA1" ];then
-	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.3:/home/gamut/Distros/apache-tomcat-8.5.40/webapps
-	sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.40/bin/startup.sh"
+if [ $ENVIRONMENT = "QA" ];then
+	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.4:/home/gamut/Distros/apache-tomcat-8.5.40/webapps
+	sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.42/bin/startup.sh"
 
-elif  [ $ENVIRONMENT = "SIT1" ];then
+elif  [ $ENVIRONMENT = "SIT" ];then
 	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.5:/home/gamut/Distros/apache-tomcat-8.5.40/webapps
-	sshpass -p "gamut" ssh gamut@172.17.0.5 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.40/bin/startup.sh"
+	sshpass -p "gamut" ssh gamut@172.17.0.5 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.42/bin/startup.sh"
 echo "deployment has been done!"
 fi
 
