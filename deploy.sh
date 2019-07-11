20421fddbf6f3ebee3d83688a089881a7b546f73
gulamd <gulamrasool78655@gmail.com>
2019-07-11 11:38:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 ##
 ##
 ENVIRONMENT=$1
-if [ $ENVIRONMENT = "QA" ];then
+if [ $ENVIRONMENT = "QA11" ];then
 	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.4:/home/gamut/Distros/apache-tomcat-8.5.40/webapps
 	sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.42/bin/startup.sh"
 
-elif  [ $ENVIRONMENT = "SIT" ];then
+elif  [ $ENVIRONMENT = "SIT11" ];then
 	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.5:/home/gamut/Distros/apache-tomcat-8.5.40/webapps
 	sshpass -p "gamut" ssh gamut@172.17.0.5 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_201" "/home/gamut/Distros/apache-tomcat-8.5.42/bin/startup.sh"
 echo "deployment has been done!"
